13157e2d6ba7a69b7c952c0d259432ee8dc796fc
Blake Griffith <blake.a.griffith@gmail.com>
2015-06-16 17:01:44 -0500

    Add local_cluster_client to __init__

--- dask/distributed/__init__.py
+++ dask/distributed/__init__.py
@@ -1,4 +1,4 @@
 from .worker import Worker
 from .scheduler import Scheduler
 from .client import Client
-from .ipython_utils import dask_client_from_ipclient
+from .ipython_utils import dask_client_from_ipclient, local_cluster_client
