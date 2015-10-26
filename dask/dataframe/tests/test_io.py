0d2035b7a60c8587e1f2ac29fbb01b92e01f5c94
Blake Griffith <blake.a.griffith@gmail.com>
2015-10-26 15:53:56 -0500

    travi-ci debugging

--- dask/dataframe/tests/test_io.py
+++ dask/dataframe/tests/test_io.py
@@ -674,20 +674,17 @@ def test_hdf_globbing():
         print("df.to_hdf(os.path.join(tdir, 'two.h5'), '/foo/data', format='table'")
         df.to_hdf(os.path.join(tdir, 'two.h5'), '/foo/data', format='table')
 
-        print("res = dd.read_hdf(os.path.join(tdir, 'one.h5'), '/*/data',
-                          chunksize=2)")
+        print("res = dd.read_hdf(os.path.join(tdir, 'one.h5'), '/*/data', chunksize=2)")
         res = dd.read_hdf(os.path.join(tdir, 'one.h5'), '/*/data',
                           chunksize=2)
         assert res.npartitions == 2
         print("tm.assert_frame_equal(res.compute(), df)")
         tm.assert_frame_equal(res.compute(), df)
 
-        print("res = dd.read_hdf(os.path.join(tdir, 'one.h5'), '/*/data',
-                          chunksize=2, start=1, stop=3)")
+        print("res = dd.read_hdf(os.path.join(tdir, 'one.h5'), '/*/data', chunksize=2, start=1, stop=3)")
         res = dd.read_hdf(os.path.join(tdir, 'one.h5'), '/*/data',
                           chunksize=2, start=1, stop=3)
-        print("expected = pd.read_hdf(os.path.join(tdir, 'one.h5'), '/foo/data',
-                               start=1, stop=3)")
+        print("expected = pd.read_hdf(os.path.join(tdir, 'one.h5'), '/foo/data', start=1, stop=3)")
         expected = pd.read_hdf(os.path.join(tdir, 'one.h5'), '/foo/data',
                                start=1, stop=3)
         print("tm.assert_frame_equal(res.compute(), expected)")
